b1196b50a93aa451317eb9a89e358c38e9393e9c
SouhailOUARGUI <ouargui550@gmail.com>
2025-05-19 09:21:24 +0100

    Initial commit

---
+++ src/main/java/com/example/gestionbanquaire/GestionbanquaireApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionbanquaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionbanquaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionbanquaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestionbanquaire/GestionbanquaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionbanquaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionbanquaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
