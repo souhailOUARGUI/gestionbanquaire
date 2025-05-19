9ed35fe219b31cf9a40305b9e16dc23d79d39fbd
SouhailOUARGUI <ouargui550@gmail.com>
2025-05-19 09:21:47 +0100

    created application properties

--- src/test/java/com/example/gestionbanquaire/GestionbanquaireApplicationTests.java
+++ src/test/java/com/example/gestionbanquaire/GestionbanquaireApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GestionbanquaireApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
